eef0913168754168185e2848fbc1e233355eb14b
Alek30k <alemank23k@gmail.com>
2024-04-01 23:28:53 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,20 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <nav>
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>AleEstate</span>
+        </a>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Agents</a>
+      </div>
+      <div className="right">
+        <a href="">Sign in</a>
+        <a href="">Sign up</a>
+      </div>
     </nav>
   );
 };
