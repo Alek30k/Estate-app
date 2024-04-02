b17cd8d3e21751ed82e8fb910fbac08ad68d9c1d
Alek30k <alemank23k@gmail.com>
2024-04-02 16:40:49 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import HomePage from "../routes/homePage/homePage";
 import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 
 function App() {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <HomePage />
     </div>
   );
 }
