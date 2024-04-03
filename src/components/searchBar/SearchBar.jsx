e6e09815f303eb3b8d3f57bc6b259ba438c79791
Alek30k <alemank23k@gmail.com>
2024-04-03 12:32:54 -0300

    last commit

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -45,7 +45,7 @@ function SearchBar() {
           placeholder="Max Price"
         />
         <button>
-          <img src="/search.png" alt="" />
+          <img src="/search.png" alt="search" />
         </button>
       </form>
     </div>
