1fc0f362da9e91f8d0b3cbee83c98053773ab823
Alek30k <alemank23k@gmail.com>
2024-04-10 15:26:59 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ListPage from "./routes/listPage/listPage";
+// import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
-import SinglePage from "./routes/singlePage/singlePage";
-import ProfilePage from "./routes/profilePage/profilePage";
-import Login from "./routes/login/login";
-import Register from "./routes/register/register";
+// import SinglePage from "./routes/singlePage/singlePage";
+// import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/Login";
+import Register from "./routes/register/Register";
+// import Login from "./routes/login/login";
+// import Register from "./routes/register/register";
 
 function App() {
   const router = createBrowserRouter([
@@ -17,18 +19,18 @@ function App() {
           path: "/",
           element: <HomePage />,
         },
-        {
-          path: "/list",
-          element: <ListPage />,
-        },
-        {
-          path: "/:id",
-          element: <SinglePage />,
-        },
-        {
-          path: "/profile",
-          element: <ProfilePage />,
-        },
+        // {
+        //   path: "/list",
+        //   element: <ListPage />,
+        // },
+        // {
+        //   path: "/:id",
+        //   element: <SinglePage />,
+        // },
+        // {
+        //   path: "/profile",
+        //   element: <ProfilePage />,
+        // },
         {
           path: "/login",
           element: <Login />,
