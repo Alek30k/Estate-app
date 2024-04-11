033ffb50bb81c1a0b022cdd5aa0709b186f8cb8d
Alek30k <alemank23k@gmail.com>
2024-04-10 21:56:01 -0300

    last commit

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,15 +1,50 @@
+import axios from "axios";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    }
+  };
+
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
+          <input
+            name="username"
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Username"
+          />
           <input name="password" type="password" placeholder="Password" />
           <button>Login</button>
+          {error && <span>{error}</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
--- src/routes/register/Register.jsx
+++ src/routes/register/Register.jsx
@@ -1,15 +1,33 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 function Register() {
-  const handleSubmit = (e) => {
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const formData = new FormData(e.target);
 
     const username = formData.get("username");
     const email = formData.get("email");
     const password = formData.get("password");
 
-    console.log(username, email, password);
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    }
   };
 
   return (
@@ -17,10 +35,11 @@ function Register() {
       <div className="formContainer">
         <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
-          <input name="password" type="text" placeholder="Username" />
+          <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
           <button>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
