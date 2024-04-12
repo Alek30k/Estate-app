cc2b45c0838dbaf05327651d1fb44809b147f5a5
Alek30k <alemank23k@gmail.com>
2024-04-12 16:34:47 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import ListPage from "./routes/listPage/listPage";
-import Layout, { RequireAuth } from "./routes/layout/layout";
-// import SinglePage from "./routes/singlePage/singlePage";
-// import ProfilePage from "./routes/profilePage/profilePage";
+import { Layout, RequireAuth } from "./routes/layout/layout";
 import Login from "./routes/login/Login";
 import Register from "./routes/register/Register";
 import ProfilePage from "./routes/profilePage/ProfilePage";
 import AboutPage from "./routes/about/page";
 import ContactPage from "./routes/contact/page";
 import AgentsPage from "./routes/agents/page";
-// import Login from "./routes/login/login";
-// import Register from "./routes/register/register";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,15 +18,6 @@ function App() {
           path: "/",
           element: <HomePage />,
         },
-        // {
-        //   path: "/list",
-        //   element: <ListPage />,
-        // },
-        // {
-        //   path: "/:id",
-        //   element: <SinglePage />,
-        // },
-
         {
           path: "/about",
           element: <AboutPage />,
--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -3,7 +3,7 @@ import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import apiRequest from "../../lib/apiRequest";
 import "./profilePage.scss";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 
 function ProfilePage() {
@@ -23,43 +23,41 @@ function ProfilePage() {
   };
 
   return (
-    currentUser && (
-      <div className="profilePage">
-        <div className="details">
-          <div className="wrapper">
-            <div className="title">
-              <h1>User Information</h1>
-              <button>Update Profile</button>
-            </div>
-            <div className="info">
-              <span>
-                Avatar:
-                <img src={currentUser.avatar || "/noavatar.jpeg"} alt="" />
-              </span>
-              <span>
-                Username: <b>{currentUser.username}</b>
-              </span>
-              <span>
-                E-mail: <b>{currentUser.email}</b>
-              </span>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-            <div className="title">
-              <h1>My List</h1>
-              <button>Create New Post</button>
-            </div>
-            <List />
-            <div className="title">
-              <h1>Saved List</h1>
-            </div>
-            <List />
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
           </div>
-        </div>
-        <div className="chatContainer">
-          <div className="wrapper">{<Chat />}</div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img src={currentUser.avatar || "/noavatar.jpeg"} alt="" />
+            </span>
+            <span>
+              Username: <b>{currentUser.username}</b>
+            </span>
+            <span>
+              E-mail: <b>{currentUser.email}</b>
+            </span>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
         </div>
       </div>
-    )
+      <div className="chatContainer">
+        <div className="wrapper">{<Chat />}</div>
+      </div>
+    </div>
   );
 }
 
