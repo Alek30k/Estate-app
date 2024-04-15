1c5b106adcea11701b52bda605c321ce8bc1a893
Alek30k <alemank23k@gmail.com>
2024-04-15 12:11:33 -0300

    last commit

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -1,4 +1,4 @@
-// import { defer } from "react-router-dom";
+import { defer } from "react-router-dom";
 import apiRequest from "./apiRequest";
 
 export const singlePageLoader = async ({ request, params }) => {
@@ -8,10 +8,8 @@ export const singlePageLoader = async ({ request, params }) => {
 
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  // const postPromise = apiRequest("/posts?" + query);
-  // return defer({
-  //   postResponse: postPromise,
-  // });}
-  const res = await apiRequest("/posts?" + params.id);
-  return res.data;
+  const postPromise = apiRequest("/posts?" + query);
+  return defer({
+    postResponse: postPromise,
+  });
 };
