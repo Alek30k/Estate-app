a00bee9a30a182f843184521721aeacbf131463a
Alek30k <alemank23k@gmail.com>
2024-04-16 10:06:23 -0300

    last commit

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./filter.scss";
+import { useSearchParams } from "react-router-dom";
 
 function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || "",
+    maxPrice: searchParams.get("maxPrice") || "",
+    bedroom: searchParams.get("bedroom") || "",
+  });
+
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
+
   return (
     <div className="filter">
       <h1>
-        Search results for <b>London</b>
+        Search results for <b>{searchParams.get("city")}</b>
       </h1>
       <div className="top">
         <div className="item">
