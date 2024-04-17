2f8c187287423e17dc18f1bc7e604d4e731cef77
Alek30k <alemank23k@gmail.com>
2024-04-17 15:02:35 -0300

    last commit

--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -75,7 +75,16 @@ function ProfilePage() {
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper">{<Chat />}</div>
+        <div className="wrapper">
+          <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.chatResponse}
+              errorElement={<p>Error loading chats!</p>}
+            >
+              {(chatResponse) => <Chat chats={chatResponse.data} />}
+            </Await>
+          </Suspense>
+        </div>
       </div>
     </div>
   );
