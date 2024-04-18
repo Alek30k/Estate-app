32303161b281e9cd0e8bd920baf6eb3517be0163
Alek30k <alemank23k@gmail.com>
2024-04-18 16:25:33 -0300

    last commit

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://estate-app-backend-rrai.onrender.com/api",
+
   withCredentials: true,
 });
 
